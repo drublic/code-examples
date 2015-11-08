f194e445ea98b2cbe1739fc48bee6b2ddd6c09a0
Hans Christian Reinl <info@drublic.de>
2015-11-08 15:00:59 +0100

    Add build tools

---
+++ build-tools/gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: ['./js/**/*.js']
+    },
+
+    sass: {
+      dist: {
+        files: [{
+          cwd: './sass/**.scss'
+        }]
+      }
+    },
+
+    watch: {
+      js: {
+        files: ['./js/**/*.js'],
+        tasks: ['jshint']
+      },
+      sass: {
+        files: ['./sass/**/*.scss'],
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+};
