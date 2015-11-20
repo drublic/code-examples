eaf1640b375656c7da97749c6b914587730961c5
Hans Christian Reinl <info@drublic.de>
2015-11-20 21:11:52 +0100

    Add more logic to casperjs tests

--- casperjs/index.js
+++ casperjs/index.js
@@ -1,4 +1,4 @@
-casper.test.begin('Google search retrieves 10 or more results', 2, function suite (test) {
+casper.test.begin('Google search retrieves 10 or more results', 4, function suite (test) {
   casper.start('http://www.google.de/', function () {
     test.assertTitle('Google', 'google homepage title is the one expected');
     test.assertExists('form[action="/search"]', 'main form is found');
@@ -6,6 +6,13 @@ casper.test.begin('Google search retrieves 10 or more results', 2, function suit
     this.fill('form[action="/search"]', {
       q: 'casperjs'
     }, true);
+  });
+
+  casper.then(function() {
+      test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
+      test.assertEval(function() {
+          return __utils__.findAll("h3.r").length >= 10;
+      }, "google search for \"casperjs\" retrieves 10 or more results");
   });
 
   casper.run(function () {
